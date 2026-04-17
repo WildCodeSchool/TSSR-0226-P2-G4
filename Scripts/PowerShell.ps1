3ac982f01e4566db0ad9364f3d4edc93845042d2
Aymeric-lc <aymeric-lc@hotmail.fr>
2026-04-17 10:04:17 +0200

    Added functions for cross-platform support

--- Scripts/PowerShell.ps1
+++ Scripts/PowerShell.ps1
@@ -53,17 +53,188 @@ function testAdd {
     } 
 }
 
-function NewLocalUsers {
+# Création de compte Windows
+
+function W_NewLocalUsers {
     testAdd
     foreach ($userName in $tableauNew) {
-        if (sshCible (Get-LocalUser -Name $userName -ErrorAction SilentlyContinue)) {
+        If (sshCible (Get-LocalUser -Name $userName -ErrorAction SilentlyContinue)) {
             Write-Host "Utilisateur $userName déjà existant"
-        }
+        } 
         else {
-            New-LocalUser -Name $userName -NoPassword
+            sshCible New-LocalUser -Name $userName -NoPassword
             Write-Host "Utilisateur $userName créé avec succès"
         }
     }
 }
 
-NewLocalUsers
+# Création de compte Linux
+
+function L_NewLocalUsers {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (grep -q "^$userName:" /etc/passwd)) {
+            Write-Host "Utilisateur $userName déjà existant"
+        } 
+        else {
+            sshCible (sudo -S adduser --allow-bad-names "$userName")
+            Write-Host "Utilisateur $userName créé avec succès"
+        }
+    }
+}
+
+
+# Changement de mot de passe Linux
+
+function L_ChangePassword {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (sudo -S grep -q "^$userName:" /etc/passwd)) {
+            sshCible (sudo -S passwd "$userName") && Write-Host "Mot de passe de $userName changé avec succès" 
+        }
+        else {
+            Write-Host "L'utilisateur $userName n'existe pas"
+        }
+    }
+}
+
+# Changement de mot de passe Windows
+
+function W_ChangePassword {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (Get-LocalUser -Name "$userName")) {
+            sshCible ($NewPwd = Read-Host -AsSecureString; Get-LocalUser -Name "$userName" | Set-LocalUser -Password $NewPwd)
+        }
+        else
+        {
+            Write-Host "L'utilisateur $userName n'existe pas"
+        }
+    }
+}
+
+# Suppression de compte Linux
+
+function L_DelUser {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (grep -q "^$userName:" /etc/passwd)) {
+            sshCible (sudo -S deluser "$userName") && Write-Host "L'utilisateur $userName à bien été supprimé"
+        }
+        else {
+            Write-Host "L'utilisateur $userName n'existe pas"
+        }
+    }
+}
+
+# Supression de compte Windows
+
+function W_DelUser {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (Get-LocalUser -Name "$userName")) {
+            sshCible (Remove-LocalUser -Name "$userName") && Write-Host "L'utilisateur $userName à bien été supprimé"
+        }    
+        else {
+            Write-Host "L'utilisateur $userName n'existe pas"
+        }
+    }
+}
+
+# Ajout à un groupe d'administration Linux
+
+function L_AddAdmin {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (grep -q "^$userName:" /etc/passwd)) {
+            sshCible (sudo -S usermod -aG sudo "$userName") && Write-Host "L'utilisateur $userName a été ajouté au groupe Admin"
+        }    
+        else {
+            Write-Host "L'utilisateur $userName n'existe pas"
+        }
+    }
+}
+
+# Ajout à un groupe d'administration Windows
+
+function W_AddAdmin {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (Get-LocalUser -Name "$userName")) {
+            sshCible (Add-LocalGroupmember -Group 'Administrators' -Member "$userName") && Write-Host "L'utilisateur $userName a été ajouté avec succès au groupe Administrateur"
+        }
+        else {
+            Write-Host "L'utilisateur $userName n'existe pas"
+        }
+    }
+}
+
+# Ajout à un groupe Linux
+
+function L_AddGroup {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (grep -q "^$userName:" /etc/passwd)) {
+            $GroupName = Read-Host "Dans quel groupe voulez-vous ajouter $userName ? "
+            If (! (sshCible (grep -q "^$groupName:" /etc/group))) {
+                $Rep = Read-Host "Le groupe choisi n'existe pas, voulez-vous le créer ? [o/n] "
+                If ($Rep -eq "o") {
+                    sshCible (sudo -S groupadd "$groupName") && Write-Host "Groupe $groupName créé"
+                    sshCible (sudo -S usermod -aG $groupName "$userName") && Write-Host "L'utilisateur $userName a été ajouté avec succès au groupe $groupName"
+                }
+                else {
+                    Write-Host "D'accord, retour au menu principal"
+                }
+            }
+            else {
+                Write-Host "L'utilisateur $userName n'existe pas"
+            }
+        }
+    }
+}
+
+#Ajout à un groupe Windows
+
+function W_AddGroup {
+    testAdd
+    foreach ($userName in $tableauNew) {
+        If (sshCible (Get-LocalUser -Name "$userName")) {
+            $GroupName = Read-Host "Dans quel groupe voulez-vous ajouter $userName ? "
+            If (! (sshCible (grep -q "^$groupName:" /etc/group))) {
+                $Rep = Read-Host "Le groupe choisi n'existe pas, voulez-vous le créer ? [o/n] "
+                If ($Rep -eq "o") {
+                    sshCible (New-LocalGroup "$groupName") && Write-Host "Groupe $groupName créé"
+                    sshCible "Add-LocalGroupmember -Group "$groupName" -Member "$userName"" && Write-Host "L'utilisateur $userName a été ajouté avec succès au groupe $groupName"
+                }            
+                else {
+                    Write-Host "D'accord, retour au menu principal"
+                }
+            }
+            else {
+                Write-Host "L'utilisateur $userName n'existe pas"
+            }
+        }
+    }
+}
+
+# Choix de redémarrage Linux
+function L_Redemarrage {
+    $Rep5 = Read-Host "$userCible@$ipCible est-ce bien la machine que vous souhaitez redémarrer ? [o/n] "
+    If ($Rep5 -eq "o") {
+        sshCible (sudo -S reboot) && Write-Host " La machine cible est en cours de redémarrage "
+    }    
+    else {
+        Write-Host "D'accord, retour au menu principal"
+    }
+}
+
+# Choix de redémarrage Windows
+function W_Redemarrage {
+    $Rep5 = Read-Host "$userCible@$ipCible est-ce bien la machine que vous souhaitez redémarrer ? [o/n] "
+    If ($Rep5 -eq "o") {
+        sshCible "Restart-Computer -ComputerName "$ipCible" -Force" && Write-Host " La machine cible est en cours de redémarrage "
+    }    
+    else {
+        Write-Host "D'accord, retour au menu principal"
+    }
+}
