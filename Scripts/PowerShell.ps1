40add8a1a14dc0828dc10a216a22d7cfa59f0274
Jacques PHAM <minjhapham@gmail.com>
2026-04-19 02:00:36 +0200

    Update PowerShell.ps1

--- Scripts/PowerShell.ps1
+++ Scripts/PowerShell.ps1
@@ -238,3 +238,217 @@ function W_Redemarrage {
         Write-Host "D'accord, retour au menu principal"
     }
 }
+
+# Création de répertoire Windows
+function w_creerDoss {
+    $absolPath = Read-Host "Où voulez-vous créer votre dossier : " 
+    if ((sshCible "Test-Path -Path '$absolPath'") -eq $false) {
+        $rep1 = Read-Host " Le chemin vers le dossier n'existe pas, voulez-vous le créer ? [o/n] "                  
+        if ("$rep1" -eq "o") {
+            $nomDoss = Read-Host "D'accord, quel est le nom du dossier à créer dans $absolPath ? " 
+            sshCible "New-Item -ItemType Directory -Path '$absolPath/$nomDoss'" 
+            Write-Host "Le dossier $nomDoss a bien été créé dans $absolPath"                                 
+        }
+        else {
+            <# Action when all if and elseif conditions are false #>
+            Write-Host "D'accord, retour au menu principal"
+        }
+    }
+    else {
+        $nomDoss = Read-Host "D'accord, quel est le nom du dossier à créer dans $absolPath ? "                 
+        sshCible "New-Item -ItemType Directory -Path '$absolPath/$nomDoss'" 
+        Write-Host "Le dossier $nomDoss a bien été créé dans $absolPath"  
+    }
+}
+function l_creerDoss {
+    $absolPath = Read-Host "Où voulez-vous créer votre dossier : " 
+    if ((sshCible "test -d '$absol_path'") -eq $false) {
+        $rep1 = Read-Host "Le chemin vers le dossier n'existe pas, voulez-vous le créer ? [o/n] " 
+        if ($rep1 -eq "o") {
+            $nomDoss = Read-Host "D'accord, quel est le nom du dossier à créer dans $absolPath ? " 
+            sshCible "sudo -S mkdir -p '$absolPath/$nomDoss'" 
+            Write-Host "Le dossier $nomDoss a bien été créé dans $absolPath"
+        }
+        else {
+            Write-Host "D'accord, retour au menu principal"
+        }
+    }
+    else {
+        $nomDoss = Read-Host "D'accord, quel est le nom du dossier à créer dans $absolPath ? " 
+        sshCible "sudo -S mkdir -p '$absolPath/$nomDoss'" 
+        Write-Host "Le dossier $nomDoss a bien été créé dans $absolPath"
+    }
+}
+# Modification de répertoire (changement de nom ou de droits d'accès) Windows
+function w_modifDoss {
+    $absolPath = Read-Host "Où se situe le dossier à modifier : " 
+    if ((sshCible "Test-Path -Path '$absolPath'") -eq $true) {
+        $ancienDoss = Read-Host " Quel est le nom du dossier à modifier dans $absolPath ? " 
+        if ((sshCible "Test-Path -Path '$absolPath\$ancienDoss'") -eq $true) {                
+            $rep4 = Read-Host " Faut-il Renommer le dossier ou en Modifier les droits ? [R/M] "                
+            if ($rep4 -eq "R") {
+                $newDoss = Read-Host "D'accord, quel est le nouveau nom du dossier ? "
+                sshCible "Rename-Item -Path '$absolPath\$ancienDoss' -NewName '$newDoss'" 
+                Write-Host "Le dossier $ancienDoss a bien été renommé en $newDoss dans $absolPath"
+            }
+            elseif ($rep4 -eq "M") { 
+                # La réponse attendue est toute attachée Sous la forme rwx ou xw ou r 
+                $chxdroit = Read-Host "Quels droits voulez vous accorder sur le dossier $ancienDoss ? [r/w/x] " 
+                                                        
+                if ("$chxdroit" -match "^[rwx]+$") {
+                    sshCible "icacls '$absolPath\$ancienDoss' /grant '${userName}:($chxdroit)'"
+                    Write-Host "Droits mis à jour pour $ancienDoss"
+                }                                        
+                else {                                            
+                    Write-Host "Ce type de droit n'existe pas"
+                }
+            }
+        }                
+            else {                    
+                Write-Host " Le dossier $ancienDoss n'existe pas "
+            } 
+    }   
+    else {        
+        Write-Host " Le chemin vers le dossier n'existe pas "
+        }
+}
+
+
+function l_modifDoss {
+    $absolPath = Read-Host "Où se situe le dossier à modifier : " 
+    if ((sshCible "test -d '$absolPath'") -eq $true) {
+        $ancienDoss = Read-Host " Quel est le nom du dossier à modifier dans $absolPath ? " 
+        if ((sshCible "test -d '$absolPath/$ancienDoss'") -eq $true) {
+            $rep4 = Read-Host " Faut-il Renommer le dossier ou en Modifier les droits ? [R/M] " 
+            if ($rep4 -eq "R") {
+                $newDoss = Read-Host "D'accord, quel est le nouveau nom du dossier ? " 
+                sshCible "sudo -S mv '$absolPath/$ancienDoss' '$absolPath/$newDoss'" 
+                Write-Host "Le dossier $ancienDoss a bien été renommé en $newDoss dans $absolPath"
+            }
+            elseif ($rep4 -eq "M") {
+                # La réponse attendue est toute attachée Sous la forme rwx ou xw ou r 
+                $chxdroit = Read-Host "Quels droits voulez vous accorder sur le dossier $ancienDoss ? [r/w/x] " 
+                if ($chxdroit -match "^[rwx]+$") {
+                    sshCible "sudo -S chmod u+$chxdroit '$absolPath/$ancienDoss'"
+                    Write-Host "Droits u+$chxdroit appliqués sur $ancienDoss"
+                }
+                else {
+                    Write-Host "Ce type de droit n'existe pas"
+                }
+            }
+        }   
+        else {   
+            Write-Host" Le dossier $ancienDoss n'existe pas "
+        }
+    }
+    else { 
+        Write-Host " Le chemin vers le dossier n'existe pas "
+    }
+}
+
+# Suppression de répertoire Windows
+function w_supprDoss {
+    $absolPath = Read-Host "Où se trouve le dossier à supprimer ? " 
+    if ((sshCible "Test-Path -Path '$absolPath'") -eq $false) {
+        $rep1 = Read-Host "Le chemin vers le dossier n'existe pas, voulez-vous rentrer un autre chemin ? [o/n] "                         
+        if ($rep1 -eq "o") {                                
+                w_supprDoss                            
+        }
+        else {                                        
+            Write-Host "D'accord, retour au menu"
+        }
+    }        
+    else {                
+        $nomDoss = Read-Host "D'accord, quel est le nom du dossier à supprimer dans $absolPath ? "
+        $fullPath = "$absolPath/$nomDoss"                         
+            if ((sshCible "Test-Path -Path '$fullPath'") -eq $true) {                                        
+                $count = sshCible "(Get-ChildItem '$fullPath' | Measure-Object).Count"
+                if ($count -eq 0) {
+                    sshCible "Remove-Item -Recurse -Force '$fullPath'" 
+                    Write-Host "Le dossier $nomDoss a bien été supprimé dans $absolPath"
+                }                                                
+                else {
+                    $rep2 = Read-Host "Le dossier choisi n'est pas vide, voulez vous continuer et supprimer son contenu ? [o/n] "                                                      
+                    if ($rep2 -eq "o") {
+                        sshCible "Remove-Item '$fullPath'" 
+                        Write-Host "Le dossier $nomDoss et son contenu ont bien été supprimés dans $absolPath"    
+                    }                                                                
+                    else {                        
+                        Write-Host "D'accord, retour au menu"
+                    }
+                }                                   
+            }
+            else { 
+                Write-Host "La valeur saisie n'existe pas ou n'est pas un dossier"
+            }        
+    } 
+}
+
+
+function l_supprDoss {
+    $absolPath = Read-Host "Où se trouve le dossier à supprimer ? " 
+    if ((sshCible "test -d '$absolPath'") -eq $false) {
+        $rep1 = Read-Host " Le chemin vers le dossier n'existe pas, voulez-vous rentrer un autre chemin ? [o/n] " 
+        if ($rep1 -eq "o") {
+            l_supprDoss
+        }
+        else {
+            Write-Host "D'accord, retour au menu"
+        }
+    }
+    else {
+        $nomDoss = Read-Host "D'accord, quel est le nom du dossier à supprimer dans $absolPath ? "
+        $fullPath = "$absolPath/$nomDoss"     
+        if ((sshCible "test -d '$absolPath/$nomDoss'") -eq $true) {
+            $isEmpty = sshCible "[ -z ""\$(ls -A '$fullPath')"" ] && echo 'vrai' || echo 'faux'" 
+            if ($isEmpty -eq "vrai") { 
+                sshCible "sudo -S rmdir '$fullPath'" 
+                Write-Host "Le dossier $nomDoss a bien été supprimé dans $absolPath"
+            }
+            else {
+                $rep2 = Read-Host "Le dossier choisi n'est pas vide, voulez vous continuer et supprimer son contenu ? [o/n] " 
+                if ($rep2 -eq "o") {
+                    sshCible "sudo -S rm -rf '$fullPath'" 
+                    Write-Host "Le dossier $nomDoss et son contenu ont bien été supprimé dans $absolPath"
+                }
+                else {
+                    Write-Host "D'accord, retour au menu"
+                }
+            }
+        }
+        else {
+            Write-Host "La valeur saisie n'existe pas ou n'est pas un dossier"
+        }
+    }
+}
+
+# Activation du Pare-feu
+function w_fireWall {
+    $rep3 = Read-Host "Voulez-vous Activer ou Désactiver le pare-feu du poste distant $ipCible ? [A/D] " 
+    if ($rep3 -eq "A") {         
+        sshCible "Set-NetFirewallProfile -Profile Domain, Private, Public -Enabled True"
+        Write-Host "Le pare-feu cible a été activé"
+    }    
+    elseif ($rep3 -eq "D") {        
+        sshCible "Set-NetFirewallProfile -Profile Domain, Private, Public -Enabled False" 
+        Write-Host "Le pare-feu cible a été désactivé"
+    }
+    else {        
+        Write-Host "Demande invalide"
+    }
+}
+
+function l_fireWall {
+    $rep3 = Read-Host "Voulez-vous Activer ou Désactiver le pare-feu du poste distant $ipCible ? [A/D] " 
+    if ($rep3 -eq "A") {
+        sshCible "sudo -S ufw --force enable" 
+        Write-Host "Le pare-feu cible a été activé"
+    }   
+    elseif ($rep3 -eq "D") {        
+        sshCible "sudo -S ufw disable" 
+        Write-Host "Le pare-feu cible a été désactivé"
+    }
+    else {    
+        Write-Host "Demande invalide"
+    }
+}
